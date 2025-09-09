116b4d5f72e56204a12b3af17054ee0a7d118e77
JoaquinPar <jparedes.peake@gmail.com>
2025-09-08 21:10:25 -0700

    Added getDetails() method to BankClient

--- src/code/ca/bcit/comp2522/bank/BankClient.java
+++ src/code/ca/bcit/comp2522/bank/BankClient.java
@@ -1,26 +1,58 @@
 package ca.bcit.comp2522.bank;
 
 public class BankClient {
-    private Name clientName;
-    private Date birthDate;
-    private Date deathDate;
-    private Date signupDate;
-    private String clientID;
+    private final Name clientName;
+    private final Date birthDate;
+    private final Date deathDate;
+    private final Date signupDate;
+    private final String clientID;
+
+    public BankClient(Name clientName, Date birthDate) {
+        this(clientName, birthDate, null, null, null);
+    }
 
     public BankClient(final Name clientName, final Date birthDate, final Date deathDate) {
+        this(clientName, birthDate, deathDate, null, null);
+    }
+
+    public BankClient(final Name clientName, final Date birthDate, final Date deathDate,
+                      final Date signupDate, final String clientID) {
         this.clientName = clientName;
         this.birthDate = birthDate;
-
-        if  (deathDate != null) {
-            this.deathDate = deathDate;
-        }
+        this.deathDate = deathDate;
+        this.signupDate = signupDate;
+        this.clientID = clientID;
     }
 
-    public String getDetils() {
-        if (deathDate != null) {
-            return clientName.toString() + " (Died " + deathDate.toString() + ") was born on" + birthDate.toString() + "!";
+    public String getDetails() {
+        String stringToReturn;
+        stringToReturn = "";
+
+        if (clientID == null) {
+            stringToReturn = stringToReturn + getClientDetails() + "\n";
+            if (deathDate == null) {
+                stringToReturn = stringToReturn + clientName.getFullName() + " (Alive) was born on "
+                        + birthDate.toString() + "!";
+            } else {
+                stringToReturn = stringToReturn + clientName.getFullName() + " (Died "
+                        + deathDate.toString() + ") was born on" + birthDate.toString() + "!";
+            }
+        } else {
+            if (deathDate == null) {
+                stringToReturn = stringToReturn + clientName.getFullName() + " client "
+                        + clientID + " (Alive) joined the bank on " + signupDate.toString();
+            } else {
+                stringToReturn = stringToReturn + clientName.getFullName() + " client "
+                        + clientID + " (Deceased) joined the bank on " + signupDate.toString();
+            }
         }
 
-        return  clientName.toString() + " (Alive) " + birthDate.toString() + "!";
+        return stringToReturn;
     }
+
+    public String getClientDetails() {
+        return clientName.getInitials() + ", " + clientName.getFullName() + ", "
+                + clientName.getReverseName();
+    }
+
 }
