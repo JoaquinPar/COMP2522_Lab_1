80f190b78f0ac7e379fbdee7c8fce8adb99d9917
knighthawk4227 <mitchellschaeffer6@gmail.com>
2025-09-10 22:45:00 -0700

    class bank account functions include deposit, withdrawUSD withdraw with pin, and get details

--- src/code/ca/bcit/comp2522/bank/BankAccount.java
+++ src/code/ca/bcit/comp2522/bank/BankAccount.java
@@ -1,4 +1,89 @@
 package ca.bcit.comp2522.bank;
 
 public class BankAccount {
+    //The mythical bank account class I will be working on
+    private final BankClient client;
+    private final Name name;
+    private double balanceUsd;
+    private final Date dateAccountOpened;
+    private final Date dateAccountClosed;
+    //if I make pin final then it can not be changed even if user forgets their pin
+    private final int pin;
+    private final String accountNumber;
+
+    public BankAccount(final BankClient client, final double balanceUsd, int pin,
+                       final String accountNumber, final Name clientName, final Date dateAccountOpened) {
+        this.client = client;
+        this.balanceUsd = balanceUsd;
+        this.name = clientName;
+        this.pin = pin;
+        Validation.validateAccountNumber(accountNumber);
+        this.accountNumber = accountNumber;
+        this.dateAccountOpened = dateAccountOpened;
+        this.dateAccountClosed = null;
+    }
+
+    public BankAccount(final BankClient client, final double balanceUsd, int pin,
+                       final String accountNumber, final Name clientName, final Date dateAccountOpened, final Date dateAccountClosed) {
+        this.client = client;
+        this.balanceUsd = balanceUsd;
+        this.name = clientName;
+        this.pin = pin;
+        Validation.validateAccountNumber(accountNumber);
+        this.accountNumber = accountNumber;
+        this.dateAccountOpened = dateAccountOpened;
+        this.dateAccountClosed = dateAccountClosed;
+    }
+
+
+    /**
+     * Amount user wants to deposit into account.
+     * @param amountToDepositUsd
+     */
+    public void deposit(final double amountToDepositUsd) {
+        balanceUsd += amountToDepositUsd;
+    }
+
+    /**
+     * Withdraw from account without the pin
+     * @param amountToWithdrawUsd amount user wants to withdraw
+     */
+    public void withdrawFromAccount(final double amountToWithdrawUsd) {
+        if (balanceUsd < amountToWithdrawUsd) {
+            throw new IllegalArgumentException("Not sufficient funds");
+        }
+        balanceUsd -= amountToWithdrawUsd;
+    }
+
+    /**
+     * Checks if the user has enough funds then their pin
+     * and then withdraws it from their account balance
+     * @param amountToWithdrawUsd Amount wanting to withdraw in USD
+     * @param pinToMatch The users pin
+     */
+    public void withdrawFromAccountWithPin(final double amountToWithdrawUsd, int pinToMatch) {
+        if (balanceUsd < amountToWithdrawUsd) {
+            throw new IllegalArgumentException("Not sufficient funds");
+        }
+        if (pinToMatch != pin) {
+            System.out.println("yo this guys pin is wrong he stealing");
+        }
+        if (pinToMatch == pin) {
+            balanceUsd -= amountToWithdrawUsd;
+        }
+    }
+
+    /**
+     * This function checks if client is dead then returns their details
+     * @return client name, balance, and when account was opened.
+     */
+    public String getDetails() {
+        if (client.isAlive()) {
+            return name.getFullName() + "had " + "$" + this.balanceUsd + " in account #: " + this.accountNumber
+                    + " Which they opened on " + this.dateAccountOpened.getDayOfWeek() + " " + this.dateAccountOpened;
+        } else {
+            return name.getFullName() + "had " + "$" + this.balanceUsd + " in account #: " + this.accountNumber
+                    + " Which they opened on " + this.dateAccountOpened + " "  + this.dateAccountOpened + " and closed on " + this.dateAccountClosed.getDayOfWeek() + " " + this.dateAccountClosed;
+        }
+    }
 }
--- src/code/ca/bcit/comp2522/bank/Validation.java
+++ src/code/ca/bcit/comp2522/bank/Validation.java
@@ -1,6 +1,9 @@
 package ca.bcit.comp2522.bank;
 
 public class Validation {
+    private static final int BANKACCOUNTNUMBERLENGTHMINSIX = 6;
+    private static final int BANKACCOUNTNUMBERLENGTHMAXSEVEN = 7;
+
     public static void validateString(final String stringToValidate, final int minStringSize, final int maxStringSize) {
         if (stringToValidate == null || stringToValidate.isBlank() ||
                 stringToValidate.contains("admin") || stringToValidate.length() < minStringSize
@@ -12,6 +15,19 @@ public class Validation {
     public static void validateNumber(final int numberToValidate, final int minNumber, final int maxNumber) {
         if (numberToValidate < minNumber || numberToValidate > maxNumber) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    /**
+     * This funcation checks the length of account number given to it
+     * to make sure that it is a valid account number
+     * @param accountToValidate This checks the account number given to it and makes
+     *                          sure it is within the parameters given for it.
+     */
+    public static void validateAccountNumber(final String accountToValidate) {
+        if (accountToValidate.length() < BANKACCOUNTNUMBERLENGTHMINSIX
+                || accountToValidate.length() > BANKACCOUNTNUMBERLENGTHMAXSEVEN) {
+            throw new IllegalArgumentException("Invalid account Numbering");
         }
     }
 }
