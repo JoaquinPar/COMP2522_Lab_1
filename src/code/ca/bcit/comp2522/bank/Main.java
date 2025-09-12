2241c4a87e52441c370ec72f500cceb5b0ab2959
JoaquinPar <jparedes.peake@gmail.com>
2025-09-11 22:51:07 -0700

    Updated main class

--- src/code/ca/bcit/comp2522/bank/Main.java
+++ src/code/ca/bcit/comp2522/bank/Main.java
@@ -2,37 +2,72 @@ package ca.bcit.comp2522.bank;
 
 public class Main {
 
-    private static final int yearOfEighteenSeventyNine = 1879;
-    private static final int dayOfFourteen = 14;
-    private static final int monthOfThree = 3;
-    private static final int yearOfNineteenFiftyFive = 1955;
-    private static final int dayOfEighteen = 18;
-    private static final int monthOfFour = 4;
-    private static final int yearOfNinteenFifty = 1950;
-    private static final int dayOfEleven = 11;
-    private static final int monthOfOne = 1;
+    //Constants for Client Albert Einstein
+    private static final int YEAR_OF_EIGHTEEN_SEVENTY_NINE = 1879;
+    private static final int FOURTEENTH_DAY = 14;
+    private static final int MONTH_OF_MARCH = 3;
+    private static final int YEAR_OF_NINETEEN_FIFTY_FIVE = 1955;
+    private static final int EIGHTEENTH_DAY = 18;
+    private static final int MONTH_OF_APRIL = 4;
+    private static final int YEAR_OF_NINETEEN_HUNDRED = 1900;
+    private static final int FIRST_DAY = 1;
+    private static final int MONTH_OF_JANUARY = 1;
+    private static final int YEAR_OF_NINETEEN_FIFTY = 1950;
+    private static final int MONTH_OF_OCTOBER = 10;
 
-    public static void main(String[] args) {
-        final Date birth;
-        final Date death;
-        final Name name;
+    private static final int YEAR_OF_NINETEEN_EIGHTEEN = 1918;
+    private static final int MONTH_OF_JULY = 7;
+    private static final int YEAR_OF_TWO_THOUSAND_THIRTEEN = 2013;
+    private static final int FIFTH_DAY = 5;
+    private static final int MONTH_OF_DECEMBER = 12;
+    private static final int YEAR_OF_NINETEEN_NINETY_FOUR = 1994;
+    private static final int TENTH_DAY = 10;
+    private static final int MONTH_OF_MAY = 5;
+
+    private static final int YEAR_OF_NINETEEN_O_SEVEN = 1907;
+    private static final int SIXTH_DAY = 6;
+    private static final int YEAR_OF_NINETEEN_FIFTY_FOUR = 1954;
+    private static final int THIRTEENTH_DAY = 13;
+    private static final int YEAR_OF_NINETEEN_FORTY = 1940;
+
+    public static void main(final String[] args) {
+        Date birth;
+        Date death;
+        Name name;
         final BankAccount bankAccount;
-        final Date signup;
-        final String clientId;
-        birth = new Date(yearOfEighteenSeventyNine, monthOfThree, dayOfFourteen);
-        death = new Date(yearOfNineteenFiftyFive, monthOfFour, dayOfEighteen);
-        signup = new Date (yearOfNinteenFifty, monthOfOne, dayOfEleven);
+        Date signup;
+        Date accountClosed;
+        String clientId;
+        BankClient client1;
+        BankClient client2;
+        BankClient client3;
+        birth = new Date(YEAR_OF_EIGHTEEN_SEVENTY_NINE, MONTH_OF_MARCH, FOURTEENTH_DAY);
+        death = new Date(YEAR_OF_NINETEEN_FIFTY_FIVE, MONTH_OF_APRIL, EIGHTEENTH_DAY);
+        signup = new Date(YEAR_OF_NINETEEN_HUNDRED, MONTH_OF_JANUARY, FIRST_DAY);
+        accountClosed = new Date (YEAR_OF_NINETEEN_FIFTY, MONTH_OF_OCTOBER, FOURTEENTH_DAY);
         name = new Name("Albert", "Einstein");
         clientId = "abc123";
 
+        client1 = new BankClient(name , birth, death, signup, accountClosed, clientId);
+        System.out.println(client1.getClientDetails());
 
-        BankClient client = new BankClient(name , birth, death, signup, clientId);
-        System.out.println(name.getInitials());
-        System.out.println(name.getFullName());
-        System.out.println(name.getReverseName());
-        System.out.println(client.getClientDetails());
+        birth = new Date(YEAR_OF_NINETEEN_EIGHTEEN, MONTH_OF_JULY, EIGHTEENTH_DAY);
+        death = new Date(YEAR_OF_TWO_THOUSAND_THIRTEEN, MONTH_OF_DECEMBER, FIFTH_DAY);
+        signup = new Date(YEAR_OF_NINETEEN_NINETY_FOUR, MONTH_OF_MAY, TENTH_DAY);
+        name = new Name("Nelson", "Mandela");
+        clientId = "654321";
 
+        client2 = new BankClient(name, birth, death, signup, clientId);
+        System.out.println(client2.getClientDetails());
+
+        birth = new Date(YEAR_OF_NINETEEN_O_SEVEN, MONTH_OF_JULY, SIXTH_DAY);
+        death = new Date(YEAR_OF_NINETEEN_FIFTY_FOUR, MONTH_OF_JULY, THIRTEENTH_DAY);
+        signup = new Date(YEAR_OF_NINETEEN_FORTY, MONTH_OF_JANUARY, FIRST_DAY);
+        accountClosed = new Date(YEAR_OF_NINETEEN_FIFTY_FOUR, MONTH_OF_JULY, THIRTEENTH_DAY);
+        name = new Name("Frida", "Kahlo");
+        clientId = "frd123";
+
+        client3 = new BankClient(name, birth, death, signup, clientId);
+        System.out.println(client3.getClientDetails());
     }
-
-
 }
